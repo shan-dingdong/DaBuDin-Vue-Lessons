250bc0f06b62582854dbd633aa660b158619b1db
shandingdong <ivy852fgh@gmail.com>
2024-06-22 19:57:52 +0800

    炫泡按鈕

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vue3GoogleLogin from 'vue3-google-login'
 const app = createApp(App);
+const GOOGLE_CLIENT_ID = '699658367864-9a7u15nl5muvsjo74n7qq3s4l5sqscqv.apps.googleusercontent.com';
 
 app.use(vue3GoogleLogin, {
-    clientId: '699658367864-9a7u15nl5muvsjo74n7qq3s4l5sqscqv.apps.googleusercontent.com'
+    clientId: GOOGLE_CLIENT_ID
   });
 
 app.mount('#app');
